e4751cd597b4a9c905a31e1107096bcd84cd5248
Segun Adeleke <oadeleke123@gmail.com>
2023-08-10 02:38:17 +0100

    Add: amenity + state modules

---
+++ models/amenity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module for Amenity
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Class for Amenity
+    Arguments:
+        name (str)
+    """
+    name = ""
---
+++ models/state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module for State
+"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Class for State
+    Arguments:
+        name (str)
+    """
+    name = ""
