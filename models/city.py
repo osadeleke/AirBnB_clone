be023ffb3914cbbf893b4671c719e84f64e2fca4
10thcode <attabadukwabena@gmail.com>
2023-08-11 09:29:05 +0000

    Add city module

---
+++ models/city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+City module
+"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """
+    Defines attributes for City object
+    """
+    state_id = ""
+    name = ""
